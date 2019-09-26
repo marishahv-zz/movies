2017238ab0e2dcac0c0cf7ea4dffca0b58091f2a
marishahv <marishahv@gmail.com>
2019-09-26 15:32:19 +0300

    fix

--- src/components/movieDetails/movieDetails.jsx
+++ src/components/movieDetails/movieDetails.jsx
@@ -3,25 +3,22 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import Logo from '../common/logo/logo';
-import mapDispatchToProps from "react-redux/es/connect/mapDispatchToProps";
 
-const movieDetails = ({posterPath, title, overview}) => {
-  return (
+const movieDetails = ({ posterPath, title, overview }) => (
+  <div>
+    <Logo />
     <div>
-      <Logo/>
+      <img src={posterPath} className="img-thumbnail img_size" alt="" />
       <div>
-        <img src={posterPath} className="img-thumbnail img_size" alt="" />
-        <div>
-          <h5 className="lead font-weight-bold">{title}</h5>
-          <p>{overview}</p>
-        </div>
+        <h5 className="lead font-weight-bold">{title}</h5>
+        <p>{overview}</p>
       </div>
     </div>
-  )
-};
+  </div>
+);
 
-const mapDispatchToProps = ({dispatc}) => ({
-  onDetailsClick: () => {dispatc()}
-})
+const mapDispatchToProps = dispatc => ({
+  onDetailsClick: () => dispatc(),
+});
 
 export default connect(null, mapDispatchToProps)(movieDetails);
